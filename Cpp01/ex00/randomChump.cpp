57849fdfb14f7626319d0e5586e868a1698ec6aa
filunie <filunie@gmail.com>
2023-02-22 12:34:09 +0100

    Cpp01, ex00 con valgrind en make

---
+++ Cpp01/ex00/randomChump.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 02:58:36 by fnieves           #+#    #+#             */
+/*   Updated: 2023/02/20 11:17:22 by fnieves          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void randomChump( std::string name )
+{
+	Zombie new_Zombie(name);
+	new_Zombie.announce();
+}
