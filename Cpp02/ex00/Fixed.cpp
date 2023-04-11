0be74fc809836733f4553944a4ddab7128a63cbe
F N <fnieves-@c3r3s2.42malaga.com>
2023-04-11 14:06:39 +0200

    comentarios cpp02

--- Cpp02/ex00/Fixed.cpp
+++ Cpp02/ex00/Fixed.cpp
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
+/*   By: fnieves- <fnieves-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 12:03:38 by fnieves           #+#    #+#             */
-/*   Updated: 2023/04/07 19:46:22 by fnieves          ###   ########.fr       */
+/*   Updated: 2023/04/11 13:17:47 by fnieves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ */
 Fixed::Fixed() : _pointInt (0)
 {
 	std::cout << "default constructor called and initialice to 0" << std::endl;
@@ -19,11 +22,32 @@ Fixed::Fixed() : _pointInt (0)
 	return;
 }
 
+// Fixed::Fixed()
+// {
+// 	std::cout << "default constructor called and initialice to 0" << std::endl;
+// 	this->_pointInt = 0;
+// 	return;
+// }
+
+/**
+ * @brief Destroy the Fixed:: Fixed object
+ * 
+ */
 Fixed::~Fixed()
 {
 	std::cout << "destructor called " << std::endl;
 	return;
 }
+
+/**
+ @brief 
+
+1. We are passing the object as a const reference because we do not
+ want to change the object
+2. We are using the *this operator to point to the current object.
+3. We are using the = operator to assign the copy to the current object.
+4. We are using the return statement to return the current object. 
+*/
 
 Fixed::Fixed(Fixed const &copy)
 {
@@ -32,6 +56,10 @@ Fixed::Fixed(Fixed const &copy)
 	return ;
 }
 
+/**
+ * @brief Get the Raw Bits object	// Why return this->_pointInt;?
+ * @return int 
+ */
 int Fixed::getRawBits() const
 {
 	std::cout << "getRawBits: " << this->_pointInt << std::endl;
@@ -45,7 +73,7 @@ void Fixed::setRawBits(int const raw)
 	return;
 }
 
-// Why return *this;?
+
 Fixed &Fixed::operator=(Fixed const &copy)
 {
 	std::cout << "operator designation " << std::endl;
--- Cpp02/ex01/Fixed.cpp
+++ Cpp02/ex01/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
+/*   By: fnieves- <fnieves-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 12:03:38 by fnieves           #+#    #+#             */
-/*   Updated: 2023/04/09 00:31:57 by fnieves          ###   ########.fr       */
+/*   Updated: 2023/04/11 14:05:29 by fnieves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,42 @@ Fixed::~Fixed()
 	return;
 }
 
-/* 
- Este constructor de copia utiliza el operador de asignación sobrecargado
- para copiar los valores del objeto copy en el objeto actual.
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ 	El valor de i se desplaza hacia la izquierda (<<) por el número de bits de fracción _nbBits,
+	lo que significa que se mueve _nbBits bits hacia la izquierda.
+	La razón para hacer esto es que _pointInt representa un valor
+	de punto fijo que tiene _nbBits bits de fracción, lo que
+	significa que los _nbBits bits menos significativos de _pointInt
+	representan la parte fraccionaria del valor de punto fijo.
+	Por lo tanto, al desplazar i hacia la izquierda por _nbBits bits,
+	se mueve la parte entera de i a la izquierda, lo que permite
+	que los bits menos significativos de _pointInt se utilicen para
+	representar la parte fraccionaria del valor de punto fijo.
+ * @param i 
  */
+Fixed::Fixed(const int i)
+{
+	this->_pointInt = i << this->_nbBits;
+	std::cout << "int constructor called and initialice to " << this->_pointInt << std::endl;
+	//bitshifting
+	return ;
+}
+
+Fixed::Fixed(const float f)
+{
+	this->_pointInt = (int)roundf(f * (1 << this->_nbBits));
+	std::cout << "float constructor called and initialice to " << this->_pointInt << std::endl;
+	return ;
+}
+
+/* 
+1. We are passing the object as a const reference because we do not
+ want to change the object
+2. We are using the *this operator to point to the current object we are creating.
+3. We are using the = operator to assign the copy to the current object.
+4. We are using the return statement to return the current object. 
+*/
 Fixed::Fixed(Fixed const &copy)
 {
 	std::cout << "Copy constructor  called " << std::endl;
@@ -64,34 +96,7 @@ Fixed& Fixed::operator=(Fixed const &copy)
 	this->_pointInt = copy.getRawBits();
 	return *this; //es este valor por referncia para devolver una clase??
 }
-/**
- * @brief Construct a new Fixed:: Fixed object
- 	El valor de i se desplaza hacia la izquierda (<<) por el número de bits de fracción _nbBits,
-	lo que significa que se mueve _nbBits bits hacia la izquierda.
-	La razón para hacer esto es que _pointInt representa un valor
-	de punto fijo que tiene _nbBits bits de fracción, lo que
-	significa que los _nbBits bits menos significativos de _pointInt
-	representan la parte fraccionaria del valor de punto fijo.
-	Por lo tanto, al desplazar i hacia la izquierda por _nbBits bits,
-	se mueve la parte entera de i a la izquierda, lo que permite
-	que los bits menos significativos de _pointInt se utilicen para
-	representar la parte fraccionaria del valor de punto fijo.
- * @param i 
- */
-Fixed::Fixed(const int i)
-{
-	std::cout << "int constructor called and initialice to " << i << std::endl;
-	//bitshifting
-	this->_pointInt = i << this->_nbBits;
-	return ;
-}
 
-Fixed::Fixed(const float f)
-{
-	std::cout << "float constructor called and initialice to " << f << std::endl;
-	this->_pointInt = (int)roundf(f * (1 << this->_nbBits));
-	return ;
-}
 
 int Fixed::toInt() const
 {
@@ -122,13 +127,30 @@ float Fixed::toFloat() const
  	operador de inserción en flujo de salida sobrecargado.
 	para la clase Fixed, utiliza la función toFloat()
  *  para obtener el valor flotante de _pointInt. Este valor fl
+This is the operation :
+	std::cout  << "a is " << a << std::endl;
+	
+Here is the explanation for the code below:
+1. We are overloading an operator in a class, so the syntax is
+		<< operator<<(std::ostream &ost,  Fixed const &fixed)
+2. The first parameter is always the ostream object that will be the left operand
+3. The second parameter is always the object that will be the right operand
+4. The first parameter is always passed by reference to avoid a copy of the object
+5. The second parameter is always passed by reference to avoid a copy of the object
+6. The second parameter is always a constant because we do not want to modify the object
+7. The return value is always an ostream object
+8. The return value is always passed by reference to avoid a copy of the object
+9. The return value is always "ost" because we want to be able to chain operators 
  * @param ost 
  * @param fixed 
  * @return std::ostream& 
  */
+
+
 std::ostream  &operator<<(std::ostream &ost,  Fixed const &fixed)
 {
 	std::cout << " Operator (<<) called with " << &ost << " and " << &fixed << std::endl;
 	ost << fixed.toFloat();
+	//we return the ostream object (means the std::cout)
 	return (ost);
 }
