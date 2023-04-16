4dd98db818659b78d973353cf91a05b7dafebc7d
filunie <filunie@gmail.com>
2023-04-16 21:08:44 +0200

    ex02 listo

---
+++ Cpp03/ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 14:37:35 by fnieves-          #+#    #+#             */
+/*   Updated: 2023/04/16 20:46:09 by fnieves          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/ClapTrap.hpp"
+#include "include/ScavTrap.hpp"
+#include "include/FragTrap.hpp"
+
+int main()
+{
+	//Check the constructors
+	FragTrap Anonimo("Anonimo");
+	Anonimo.printClass();
+	FragTrap Joe("Joe");
+	Joe.printClass();
+	FragTrap Jander (Joe);
+	Jander.printClass();
+	FragTrap Manu ("Manu");
+	Anonimo = Joe;
+	Anonimo.printClass();
+
+	
+	
+	//*****************
+	Joe.attack(Jander.getName());
+	Joe.takeDamage(4);
+	Joe.printClass();
+	Joe.beRepaired(2);
+	std::cout << "\n*****After attacking**********"  << std::endl;
+	Joe.printClass();
+	return 0;
+}
---
+++ Cpp03/ex02/src/ClapTrap.cpp
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 12:30:42 by fnieves-          #+#    #+#             */
+/*   Updated: 2023/04/16 20:44:04 by fnieves          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/ClapTrap.hpp"
+
+/* ************************************************************************** */
+/* CONSTRUCTORS                                                               */
+/* ************************************************************************** */
+
+ClapTrap::ClapTrap() : _Name("Default") , _pointsHit(10) , _pointsEnergy(10) , _pointsAttack(0)
+{
+	std::cout << "Constructor default ClapTrap called whose name is " << this->_Name << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string const name) : _Name(name) , _pointsHit(10) , _pointsEnergy(10) , _pointsAttack(0)
+{
+	std::cout << "Constructor default ClapTrap called whose name is " << this->_Name << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+{
+	*this = copy;
+	std::cout << "Constructor copy ClapTrap called and all atributes copied. Name of the original: " << 
+	copy.getName() << ". Name of copy: " << this->_Name  
+	<< std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor default ClapTrap called for " << this->_Name << std::endl;
+}
+
+
+ClapTrap & ClapTrap::operator=(const ClapTrap &src)
+{
+	std::cout << "Operator asignation ClapTrap from Instance: " << src._Name << std::endl;
+	if (this == &src)
+		return (*this);
+	this->_Name = src._Name;
+	this->_pointsHit = src._pointsHit;
+	this->_pointsEnergy = src._pointsEnergy;
+	this->_pointsAttack = src._pointsAttack;
+	//std::cout << "Operator asignation ClapTrap from Instance: " << src._Name << std::endl;
+	return (*this);
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (this->_pointsEnergy >= 1)
+	{
+		std::cout << this->_Name << " ClapTrap is attacking to: "  << target << " with points atttack " 
+		<< this->_pointsAttack << std::endl;
+		this->_pointsEnergy--;
+	}
+	else
+		std::cout << this->_Name << " has no energy to attack to " << target << std::endl;
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_pointsHit >= amount)
+	{
+		std::cout << this->_Name << " ClapTrap takeDamage with : " << amount 
+		<< " points."  <<   std::endl;
+		this->_pointsHit -= amount;
+	}
+	else
+		std::cout << this->_Name << " ClapTrap  can not takeDamage with : " << amount 
+		<< " points."  <<   std::endl;
+
+		
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_pointsEnergy >= 1)
+	{		
+		std::cout << this->_Name << " ClapTrap  is repaired by : " << amount 
+		<< " points."  <<   std::endl;
+		this->_pointsEnergy -= 1;
+		this->_pointsHit += amount;
+	}
+	else
+		std::cout << this->_Name << " has no energy enough to beRepaired ClapTrap " << this->_Name << std::endl;
+}
+
+const std::string ClapTrap::getName() const
+{
+	return (this->_Name);
+}
+
+void  ClapTrap::printClass() const
+{
+	std::cout <<  "\n**************\n"
+	<< "Properties from class: " << this->_Name
+	<<  "\nEnergy: " << this->_pointsEnergy
+	<<  "\nHitPoints: " << this->_pointsHit
+	<<  "\nAtack: " << this->_pointsAttack
+	<<  "\n**************\n "
+	<< std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+// {
+// private:
+// 	/* data */
+// public:
+// 	ClapTrap(/* args */);
+// 	~ClapTrap();
+// };
+
+// ClapTrap::ClapTrap(/* args */)
+// {
+// }
+
+// ClapTrap::~ClapTrap()
+// {
+// }
