49eb3601ae5b0516be96d5d004d9fa995318c60b
filunieto <filunie@gmail.com>
2023-06-22 11:16:28 +0200

    nuevos archivs creados y probando gt desde windows bash

--- Cpp06/ex00/main.cpp
+++ Cpp06/ex00/main.cpp
@@ -6,14 +6,20 @@
 /*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/18 13:33:01 by fnieves           #+#    #+#             */
-/*   Updated: 2023/06/18 13:53:54 by fnieves          ###   ########.fr       */
+/*   Updated: 2023/06/20 21:22:58 by fnieves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "include/Character.hpp"
 
+#include <iostream>
 
-int main()
+int main(int argc, char**argv)
 {
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./convert <literal>" << std::endl;
+		return 1;
+	}
 	return 0;
 }
